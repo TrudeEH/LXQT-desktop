99961e522f87b60596d101a656f579c091666402
trudertube <trudertube@gmail.com>
2021-02-06 21:06:57 +0000

    lxpanel

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # Desktop:
-apt install -y openbox obconf obmenu lightdm pcmanfm git dmenu feh compton lxappearance firefox-esr mate-wallpapers breeze-gtk-theme menu lxpanel network-manager-applet flameshot #[ou tint2] lxterminal
+sudo apt install -y openbox obconf obmenu lightdm pcmanfm git dmenu feh compton lxappearance firefox-esr breeze-cursor-theme breeze-gtk-theme menu lxpanel nm-tray flameshot #[ou tint2] lxterminal
 
 # Desktop Config:
 mkdir ~/.config/ &> /dev/null
@@ -8,14 +8,16 @@ mkdir ~/.config/compton &> /dev/null
 mkdir ~/.config/tint2 &> /dev/null
 mkdir ~/.config/TrudeOS &> /dev/null
 
-rm ~/.config/openbox/* &> /dev/null
-rm ~/.config/compton/* &> /dev/null
-rm ~/.config/tint2/* &> /dev/null
-rm ~/.config/TrudeOS/* &> /dev/null
+rm -r ~/.config/openbox/* &> /dev/null
+rm -r ~/.config/compton/* &> /dev/null
+rm -r ~/.config/tint2/* &> /dev/null
+rm -r ~/.config/TrudeOS/* &> /dev/null
+rm -r ~/.config/lxpanel/ &> /dev/null
 
 cp autostart ~/.config/openbox/
 cp tint2rc ~/.config/tint2/
 cp wallpaper.jpeg ~/.config/TrudeOS/
+cp -r lxpanel ~/.config/
 
 git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git
 cd Tela-circle-icon-theme ; ./install.sh ; cd .. ; rm -rf Tela-circle-icon-theme
