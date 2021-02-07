ffcbe06516b4f2a86f8567b03205ad97104810f7
trude <trude@trude-desk.lan>
2021-02-07 23:08:41 +0000

    TrudeOS v1.0

--- install.sh
+++ install.sh
@@ -1,29 +1,17 @@
-# Desktop:
-sudo apt install -y openbox obconf obmenu lightdm pcmanfm git dmenu feh compton lxappearance firefox-esr breeze-cursor-theme breeze-gtk-theme menu lxpanel nm-tray flameshot #[ou tint2] lxterminal
+#! /bin/bash
 
-# Desktop Config:
-mkdir ~/.config/ &> /dev/null
-mkdir ~/.config/openbox/ &> /dev/null
-mkdir ~/.config/compton &> /dev/null
-mkdir ~/.config/tint2 &> /dev/null
-mkdir ~/.config/TrudeOS &> /dev/null
+sudo apt install lxde flameshot -y
 
-rm -r ~/.config/openbox/* &> /dev/null
-rm -r ~/.config/compton/* &> /dev/null
-rm -r ~/.config/tint2/* &> /dev/null
-rm -r ~/.config/TrudeOS/* &> /dev/null
-rm -r ~/.config/lxpanel/ &> /dev/null
+mkdir ~/.TrudeOS
+cp wallpaper.jpeg ~/.TrudeOS
+cp menu-icon.png ~/.TrudeOS
 
-cp autostart ~/.config/openbox/
-cp tint2rc ~/.config/tint2/
-cp wallpaper.jpeg ~/.config/TrudeOS/
+rm -r ~/.config/lxpanel
+rm -r ~/.config/lxsession
+rm -r ~/.config/lxterminal
+rm -r ~/.config/openbox
+
 cp -r lxpanel ~/.config/
-
-git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git
-cd Tela-circle-icon-theme ; ./install.sh ; cd .. ; rm -rf Tela-circle-icon-theme
-
-#Themes:
-# Os temas não são aplicados automaticamente (ainda)
-# Como aplicar os temas:
-# 1 - obconf -> instalar o tema de janelas
-# 2 - lxappearance -> mudar os temas/instalar  o tema de ícones
+cp -r lxsession ~/.config/
+cp -r lxterminal ~/.config/
+cp -r openbox ~/.config/
