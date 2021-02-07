c73da5466b05ba6e94cd3dd88132a994a9a26786
trude <trude@trude-desk.lan>
2021-02-07 23:18:16 +0000

    TrudeOS v1.0.1

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
-sudo apt install lxde flameshot -y
+# LXDE + Themes install
+sudo apt install -y lxde flameshot breeze-cursor-theme breeze-gtk-theme firefox-esr
 
+# OS config
 mkdir ~/.TrudeOS
 cp wallpaper.jpeg ~/.TrudeOS
 cp menu-icon.png ~/.TrudeOS
 
+# DE config
 rm -r ~/.config/lxpanel
 rm -r ~/.config/lxsession
 rm -r ~/.config/lxterminal
@@ -15,3 +18,7 @@ cp -r lxpanel ~/.config/
 cp -r lxsession ~/.config/
 cp -r lxterminal ~/.config/
 cp -r openbox ~/.config/
+
+# Tela Circle Icon Theme
+git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git
+cd Tela-circle-icon-theme ; ./install.sh ; cd .. ; rm -rf Tela-circle-icon-theme
