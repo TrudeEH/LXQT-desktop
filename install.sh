39cdb404fa9e43a83432ef40579d00fe41fa2264
trudertube <trudertube@gmail.com>
2021-02-10 21:33:41 +0000

    xscreensaver settings

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ rm -r ~/.config/openbox
 rm -r ~/.config/pcmanfm-qt
 rm -r ~/.config/qterminal.org
 rm -r ~/.config/compton.conf
+rm ~/.xscreensaver
 
 cp -r gtk-3.0 ~/.config/
 cp -r lxqt ~/.config/
@@ -19,6 +20,7 @@ cp -r openbox ~/.config/
 cp -r pcmanfm-qt ~/.config/
 cp -r qterminal.org ~/.config/
 cp -r compton.conf ~/.config/
+cp xscreensaver ~/.xscreensaver
 
 # Tela Circle Icon Theme
 git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git
