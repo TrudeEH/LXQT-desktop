4e2aee585fce2edf0fc1b8f9033bf395ab4a5266
trudertube <trudertube@gmail.com>
2021-02-17 09:28:27 +0000

    DM config

--- install.sh
+++ install.sh
@@ -5,14 +5,42 @@
 #   by TrudeEH #
 ################
 
+# Update base:
+echo
+echo "[+] Updating the system base..."
+echo -ne '[.........................]\r'
+sudo apt update -y &> /dev/null
+echo -ne '[#####....................]\r'
+sudo apt upgrade -y &> /dev/null
+echo -ne '[##########...............]\r'
+sudo apt dist-upgrade -y &> /dev/null
+echo -ne '[################.........]\r'
+sudo apt autoremove -y &> /dev/null
+echo -ne '[#####################....]\r'
+sudo apt autoclean -y &> /dev/null
+echo -ne '[#########################]\r'
+echo -ne '\n'
+echo
+# --------------------------------------------------
+
+# Display Manager config
+echo "------------------------------------------------"
+echo "Please select slim as the default window manager"
+echo "------------------------------------------------"
+sleep 5000
+
+sudo apt install slim -y
+
 # DE config
-rm -r ~/.config/gtk-3.0
-rm -r ~/.config/lxqt
-rm -r ~/.config/openbox
-rm -r ~/.config/pcmanfm-qt
-rm -r ~/.config/qterminal.org
-rm -r ~/.config/compton.conf
-rm ~/.xscreensaver
+rm -r ~/.config/gtk-3.0 &> /dev/null
+rm -r ~/.config/lxqt &> /dev/null
+rm -r ~/.config/openbox &> /dev/null
+rm -r ~/.config/pcmanfm-qt &> /dev/null
+rm -r ~/.config/qterminal.org &> /dev/null
+rm -r ~/.config/compton.conf &> /dev/null
+rm ~/.xscreensaver &> /dev/null
+sudo rm -rf /usr/share/slim/themes/tslim &> /dev/null
+sudo rm /etc/slim.conf &> /dev/null
 
 cp -r gtk-3.0 ~/.config/
 cp -r lxqt ~/.config/
@@ -21,6 +49,8 @@ cp -r pcmanfm-qt ~/.config/
 cp -r qterminal.org ~/.config/
 cp -r compton.conf ~/.config/
 cp xscreensaver ~/.xscreensaver
+sudo cp -r tslim/ /usr/share/slim/themes
+sudo cp slim.conf /etc/slim.conf
 
 # Icons
 sudo rm -r /usr/share/TrudeOS
